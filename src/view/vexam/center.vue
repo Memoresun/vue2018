94f923b180360847287cb75ffca3d280cc5436e5
memoresun <memoresun@gmail.com>
2018-03-08 00:30:08 +0800

    vexam center 页面

--- src/view/vexam/center.vue
+++ src/view/vexam/center.vue
@@ -1,3 +1,54 @@
 <template>
-  <div>中心</div>
+  <div class="app-root">
+    <div class="center-header">
+      <div class="center-header-image">
+        <img src="" alt="">
+      </div>
+      <div class="center-header-name">小雨哗啦啦</div>
+    </div>
+    <div class="center-content">
+      <div class="weui-cells">
+        <a class="weui-cell weui-cell_access" href="#">
+          <div class="weui-cell__hd"> 
+            <img class="www-cell-icon" src="../../assets/images/QZBKV1/c_1.png" alt="">
+          </div>
+          <div class="weui-cell__bd weui-cell_primary">
+            <p class="www-cell-font">我的收藏</p>
+          </div>
+          <span class="weui-cell__ft"></span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="#">
+          <div class="weui-cell__hd" >
+            <img class="www-cell-icon" src="../../assets/images/QZBKV1/c_2.png" alt="">
+          </div>
+          <div class="weui-cell__bd weui-cell_primary">
+            <p class="www-cell-font">我的资料</p>
+          </div>
+          <span class="weui-cell__ft"></span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="#">
+          <div class="weui-cell__hd">
+            <img class="www-cell-icon" src="../../assets/images/QZBKV1/c_3.png" alt="">
+          </div>
+          <div class="weui-cell__bd weui-cell_primary">
+            <p class="www-cell-font">我的报名</p>
+          </div>
+          <span class="weui-cell__ft"></span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="#">
+          <div class="weui-cell__hd">
+            <img class="www-cell-icon" src="../../assets/images/QZBKV1/c_4.png" alt="">
+          </div>
+          <div class="weui-cell__bd weui-cell_primary">
+            <p class="www-cell-font">我的VIP</p>
+          </div>
+          <span class="weui-cell__ft"></span>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style type="text/css">
+
+</style>
